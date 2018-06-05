fdf979526860916b197c37f90645964c9a209258
Steph <N/A>
2018-06-04 22:06:15 -0400

    Optimized loader section

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+const buildLoaderItems = () => {
+  const items = [];
+  for (let i = 0; i < 5; i++) {
+    items.push(<div className={`loader__rect${i+1}`} key={i}></div>);
+  }
+  return items;
+}
+
 const LoadingPage = props => (
   <div className="loader">
     <div className="loader__item">
-      <div className="loader__rect1"></div>
-      <div className="loader__rect2"></div>
-      <div className="loader__rect3"></div>
-      <div className="loader__rect4"></div>
-      <div className="loader__rect5"></div>
+      {buildLoaderItems()}
       {props.message && <h3 className="loader__message">{props.message}</h3>}
     </div>
   </div>
